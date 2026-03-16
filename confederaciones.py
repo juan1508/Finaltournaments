67f6fe0b44db154a7a551ea38096b709693b1f84
juan1508 <66849888+juan1508@users.noreply.github.com>
2026-03-15 22:22:49 -0500

    Update confederaciones.py

--- confederaciones.py
+++ confederaciones.py
@@ -1,10 +1,6 @@
 """
 pages/confederaciones.py - Copa África (CAF), Copa Oro (CONCACAF), Copa Asia (AFC)
 """
-import sys, os as _os
-_root = _os.path.dirname(_os.path.dirname(_os.path.abspath(__file__)))
-if _root not in sys.path:
-    sys.path.insert(0, _root)
 import streamlit as st
 import random
 from data import CAF_TEAMS, CONCACAF_TEAMS, AFC_TEAMS
